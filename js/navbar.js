df61910bcb81c651ce068b461fa44cecaf9ac325
FISC <ahjkdfgghcvnfdsabjfdnb@bfdkjsdfkjnkjdgf.com>
2025-12-01 13:11:09 -0500

    .html colocados

---
+++ js/navbar.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarContainer = document.getElementById("navbar");
+
+  if (navbarContainer) {
+    fetch("navbar.html")
+      .then(response => response.text())
+      .then(html => {
+        navbarContainer.innerHTML = html;
+      })
+      .catch(error => {
+        console.error("Error al cargar el navbar:", error);
+      });
+  }
+});
